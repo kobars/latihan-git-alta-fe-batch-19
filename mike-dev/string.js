5b91f7af2ddb358d8285605d35005b73cfa501a4
kristianmikhael667 <kristianmikhael667@gmail.com>
2024-03-06 20:36:25 +0700

    result exercise algorithm stirng, number, and boolean

---
+++ mike-dev/string.js
@@ -0,0 +1,15 @@
+// Concatenate
+let firstName = "Mikhael";
+let lastName = "Kristian";
+
+var fullName = firstName + lastName;
+console.log("FullName without space ", fullName);
+
+// Get the length
+let name = "Angel S";
+
+let lengthOfName_1 = name.length;
+console.log("LengthOfName version 1 : ", lengthOfName_1);
+
+let lengthOfName_2 = "Ada Lovelace".length;
+console.log("LengthOfName version 2 : ", lengthOfName_2);
