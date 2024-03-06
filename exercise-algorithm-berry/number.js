8bb40b0c9c999a9ab5d1fb6569fbc4c94e2260fb
Berry Kurniawan <berykurniawan46@gmail.com>
2024-03-06 20:41:22 +0700

    result exercise string, number, boolean

---
+++ exercise-algorithm-berry/number.js
@@ -0,0 +1,18 @@
+let message = "hello";
+message = 123456;
+
+let n = 123;
+n = 12.345;
+let integer = 4;
+let float = 4.7;
+let max = +Infinity;
+
+let sum = 4 + 5;
+let difference = 9 - 4;
+let product = 4 * 5;
+let quotient = 20 / 4;
+let remainder = 9 % 2;
+
+console.log(1 / 0);
+console.log(Infinity);
+console.log("not a number" / 2);
