39d5689dcf48013172c4fd980b93f55a22ab4cf6
moesvck <m.imamzarkasyi96@gmail.com>
2024-03-06 20:53:12 +0700

    exercise-algorithm-imam

---
+++ exercise-algorithm-imam/boolean.js
@@ -0,0 +1,5 @@
+let nameFieldChecked = true;
+let ageFieldChecked = false;
+
+let isGreater = 4 > 1;
+console.log(isGreater);
---
+++ exercise-algorithm-imam/string.js
@@ -0,0 +1,15 @@
+let firstName = 'Robert';
+let lastName = 'Smalls';
+
+var fullName = firstName + lastName;
+console.log('fullName without space :', fullName);
+
+//get the Length
+let name = 'Ada lovelace';
+
+let lengthOfName_1 = name.length;
+console.log('lengthOfName version 1:', lengthOfName_1);
+
+//OR
+let lengthOfName_2 = 'Ada lovelace'.length;
+console.log('lengthOfName version 2:', lengthOfName_2);
